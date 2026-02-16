deb6fb0a27ce8a8e63ca02b1ce68a2c3f5c3c897
vishal <rautv2005@gmail.com>
2026-02-16 22:44:25 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -8,5 +8,4 @@ const rl = readline.createInterface({
 // TODO: Uncomment the code below to pass the first stage
 rl.question("$ ", (answer) => {
   console.log(`${command}: command not found`);
-  
 });
