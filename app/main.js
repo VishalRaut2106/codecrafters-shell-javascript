1b783805b148255023c5c4d877e5b5c316e4186c
vishal <rautv2005@gmail.com>
2026-03-03 22:40:00 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,37 +1,49 @@
-const readline = require("readline");
+const readline = require('readline')
+const { spawnSync } = require('child_process')
+
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-rl.setPrompt("$ ");
-rl.on("line", (command) => {
-  executeCommand(command);
-  if (!rl.closed) {
-    rl.prompt();
+	input: process.stdin,
+	output: process.stdout
+})
+
+let recognizedCommands = {
+	echo: (cmds) => console.log(cmds.slice(1).join(' ')),
+	exit: (cmds) => process.exit(cmds[1]),
+  type: (cmds) => type(cmds)
+}
+
+let executableInPath = (cmd) => {
+  const res = spawnSync('which', [cmd])
+  if (res.status === 0) return res.stdout.toString().replace(/(\r\n|\n|\r)/gm, '')
+}
+
+let type = (cmds) => {
+  let executable = executableInPath(cmds[1])
+  let builtin = isValidCommand(cmds[1], recognizedCommands)
+  if (executable || builtin) {
+    console.log(`${cmds[1]} is ${builtin ? 'a shell builtin' : executable}`)
   }
-});
-function executeCommand(command) {
-  const parts = command.trim().split(" ");
-  const cmd = parts[0];
-  const args = parts.slice(1).join(" ");
-  if (commands[cmd]) {
-    commands[cmd](args);
-  } else {
-    console.log(`${command}: not found`);
+  else {
+    console.log(`${cmds.slice(1).join(' ')}: not found`)
   }
 }
-const commands = {
-  echo: (input) => {
-    console.log(input.toString().split(",").join(" "));
-  },
-  type: (input) => {
-    commands[input]
-      ? console.log(`${input} is a shell builtin`)
-      : console.log(`${input}: not found`);
-  },
-  exit: () => {
-    rl.close();
-    return;
-  },
-};
-rl.prompt();
+
+rl.setPrompt('$ ')
+rl.prompt()
+
+rl.on('line', (input) => {
+
+	let inputCommands = input.split(' ')
+	let directive = inputCommands[0]
+
+	isValidCommand(directive, recognizedCommands)
+		? recognizedCommands[directive](inputCommands)
+		: console.log(`${input}: command not found`)
+
+  rl.prompt()
+  
+})
+
+let isValidCommand = (cmd, commandList) => {
+	return commandList.hasOwnProperty(cmd)
+}
