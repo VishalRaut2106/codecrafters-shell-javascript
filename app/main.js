729c7a430f4950348fc292634f30a62b91cb25c5
vishal <rautv2005@gmail.com>
2026-03-03 22:06:56 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -3,10 +3,16 @@ const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+  prompt: "$ ",
 });
 
 // TODO: Uncomment the code below to pass the first stage
-rl.question("$ ", (answer) => {
-  console.log(`${answer}: command not found`)
-  rl.prompt();
-});
+function repl() {
+  rl.prompt();  
+  rl.once("line", (line) => {
+    console.log(`${line}:command not found`);
+    repl();
+  });
+}
+repl();
+    
