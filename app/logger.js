24372544decda24add7fe8798c6dadfb726e047b
visha raut <rautv2005@gmail.com>
2026-05-11 00:31:41 +0530

    codecrafters submit [skip ci]

--- app/logger.js
+++ app/logger.js
@@ -4,6 +4,9 @@ function log(message, fd) {
   if (typeof fd === "number" && fd > 0) {
     // Write to file descriptor
     fs.writeSync(fd, message + "\n");
+  } else if (fd && typeof fd.write === "function") {
+    // Write to stream
+    fd.write(message + "\n");
   } else {
     // Write to stdout
     console.log(message);
@@ -14,6 +17,9 @@ function error(message, fd) {
   if (typeof fd === "number" && fd > 0) {
     // Write to file descriptor
     fs.writeSync(fd, message + "\n");
+  } else if (fd && typeof fd.write === "function") {
+    // Write to stream
+    fd.write(message + "\n");
   } else {
     // Write to stderr
     console.error(message);
