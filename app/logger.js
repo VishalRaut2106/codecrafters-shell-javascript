a21a130805b400fa98b2a4dcebb061a0429d6d29
visha raut <rautv2005@gmail.com>
2026-05-11 00:23:39 +0530

    codecrafters submit [skip ci]

---
+++ app/logger.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+function log(message, fd) {
+  if (typeof fd === "number" && fd > 0) {
+    // Write to file descriptor
+    fs.writeSync(fd, message + "\n");
+  } else {
+    // Write to stdout
+    console.log(message);
+  }
+}
+
+function error(message, fd) {
+  if (typeof fd === "number" && fd > 0) {
+    // Write to file descriptor
+    fs.writeSync(fd, message + "\n");
+  } else {
+    // Write to stderr
+    console.error(message);
+  }
+}
+
+module.exports = { log, error };
---
+++ app/utility.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const os = require("os");
+
+function tokenizeCommand(command) {
+  const tokens = [];
+  let currentToken = "";
+  let inDoubleQuotes = false;
+  let inSingleQuotes = false;
+
+  for (let i = 0; i < command.length; i++) {
+    const char = command[i];
+    const nextChar = command[i + 1];
+
+    if (char === '"' && !inSingleQuotes) {
+      inDoubleQuotes = !inDoubleQuotes;
+      currentToken += char;
+    } else if (char === "'" && !inDoubleQuotes) {
+      inSingleQuotes = !inSingleQuotes;
+      currentToken += char;
+    } else if ((char === " " || char === "\t") && !inDoubleQuotes && !inSingleQuotes) {
+      if (currentToken) {
+        tokens.push(currentToken);
+        currentToken = "";
+      }
+    } else {
+      currentToken += char;
+    }
+  }
+
+  if (currentToken) {
+    tokens.push(currentToken);
+  }
+
+  return tokens;
+}
+
+function groupTokens(tokens) {
+  const groups = [];
+  let currentGroup = [];
+
+  for (const token of tokens) {
+    if (token === "|") {
+      if (currentGroup.length > 0) {
+        groups.push(currentGroup);
+        currentGroup = [];
+      }
+    } else {
+      currentGroup.push(token);
+    }
+  }
+
+  if (currentGroup.length > 0) {
+    groups.push(currentGroup);
+  }
+
+  return groups;
+}
+
+function computeAbsolutePath(pathStr) {
+  if (!pathStr) {
+    return process.cwd();
+  }
+
+  // Handle home directory
+  if (pathStr.startsWith("~")) {
+    pathStr = pathStr.replace("~", os.homedir());
+  }
+
+  // If already absolute, return as is
+  if (path.isAbsolute(pathStr)) {
+    return pathStr;
+  }
+
+  // Resolve relative to current working directory
+  return path.resolve(process.cwd(), pathStr);
+}
+
+module.exports = {
+  tokenizeCommand,
+  groupTokens,
+  computeAbsolutePath,
+};
