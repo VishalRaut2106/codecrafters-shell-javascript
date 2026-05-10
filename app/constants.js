efc9fc8611dc34ff45a9656b61058c30d1c2985b
visha raut <rautv2005@gmail.com>
2026-05-11 00:32:41 +0530

    codecrafters submit [skip ci]

--- app/constants.js
+++ app/constants.js
@@ -2,31 +2,7 @@ const fs = require("fs");
 
 const BUILTIN_COMMANDS = ["exit", "pwd", "cd", "echo", "type", "history"];
 
-// External commands - would be populated from system PATH
+// External commands will be discovered dynamically
 const EXTERNAL_COMMANDS = {};
-
-// Populate EXTERNAL_COMMANDS from system PATH
-const pathEnv = process.env.PATH;
-if (pathEnv) {
-  const paths = pathEnv.split(process.platform === "win32" ? ";" : ":");
-  for (const dir of paths) {
-    try {
-      const files = fs.readdirSync(dir);
-      for (const file of files) {
-        // Add command without extension on Unix, with common extensions on Windows
-        if (process.platform === "win32") {
-          if (file.endsWith(".exe") || file.endsWith(".bat") || file.endsWith(".cmd")) {
-            const cmdName = file.substring(0, file.lastIndexOf("."));
-            EXTERNAL_COMMANDS[cmdName] = `${dir}/${file}`;
-          }
-        } else {
-          EXTERNAL_COMMANDS[file] = `${dir}/${file}`;
-        }
-      }
-    } catch (err) {
-      // Ignore errors reading directories
-    }
-  }
-}
 
 module.exports = { BUILTIN_COMMANDS, EXTERNAL_COMMANDS };
--- app/readline/completer.js
+++ app/readline/completer.js
@@ -1,7 +1,50 @@
+const fs = require("fs");
+const path = require("path");
 const { KNOWN_COMMANDS } = require("../functions/type");
 
+let cachedCommands = null;
+let lastPathEnv = null;
+
+function getAvailableCommands() {
+  const pathEnv = process.env.PATH;
+  
+  // Only refresh if PATH has changed or cache is empty
+  if (cachedCommands && pathEnv === lastPathEnv) {
+    return cachedCommands;
+  }
+  
+  const commands = new Set(KNOWN_COMMANDS);
+  lastPathEnv = pathEnv;
+  
+  if (pathEnv) {
+    const paths = pathEnv.split(process.platform === "win32" ? ";" : ":");
+    for (const dir of paths) {
+      try {
+        const files = fs.readdirSync(dir);
+        for (const file of files) {
+          // Add command without extension on Unix, with common extensions on Windows
+          if (process.platform === "win32") {
+            if (file.endsWith(".exe") || file.endsWith(".bat") || file.endsWith(".cmd")) {
+              const cmdName = file.substring(0, file.lastIndexOf("."));
+              commands.add(cmdName);
+            }
+          } else {
+            commands.add(file);
+          }
+        }
+      } catch (err) {
+        // Ignore errors reading directories
+      }
+    }
+  }
+  
+  cachedCommands = Array.from(commands).sort();
+  return cachedCommands;
+}
+
 const completer = (line) => {
-  let hits = KNOWN_COMMANDS.filter((cmd) => cmd.startsWith(line));
+  const availableCommands = getAvailableCommands();
+  let hits = availableCommands.filter((cmd) => cmd.startsWith(line));
   hits = hits.map((row) => row + " ");
   if (hits.length === 0) {
     process.stdout.write("\x07"); // bell
